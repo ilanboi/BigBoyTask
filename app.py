712a868bc339d338de63bacf6771b5c531ba483b
ilanboi <97630631+ilanboi@users.noreply.github.com>
2022-01-13 01:48:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from aws_cdk import (
     core,
     aws_s3 as s3,
-    aws_cloudfront as cf,
-    custom_resources as cr
+    aws_cloudfront as cloudfront
 )
 
 class StaticExampleStack(core.Stack):
@@ -10,29 +9,19 @@ class StaticExampleStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
  
-        bucket = s3.Bucket(self, "MyBucket",
+        self.bucket = s3.Bucket(self, "MyBucket",
                            access_control=s3.BucketAccessControl.PUBLIC_READ,
                            website_index_document="index.html"
                            )
         bucket.grant_public_access()
  
-        cf.CloudFrontWebDistribution(self, "CDN",
-                                     price_class=cf.PriceClass.PRICE_CLASS_100,
-                                     alias_configuration=cf.AliasConfiguration(
-                                         names=["static.rubenjgarcia.es"],
-                                         acm_cert_ref=parameter.get_data_string("Parameter.Value"),
-                                         ssl_method=cf.SSLMethod.SNI,
-                                         security_policy=cf.SecurityPolicyProtocol.TLS_V1_1_2016
-                                     ),
-                                     origin_configs=[
-                                         cf.SourceConfiguration(
-                                             behaviors=[
-                                                 cf.Behavior(
-                                                     is_default_behavior=True)
-                                             ],
-                                             s3_origin_source=cf.S3OriginConfig(
-                                                 s3_bucket_source=bucket
-                                             )
-                                         )
-                                     ]
-                                     )
+        self.distribution = cloudfront.Distribution(
+            self,
+            "cloudfront_distribution",
+            default_behavior=cloudfront.BehaviorOptions(
+                origin=origins.S3Origin(self.bucket),
+                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+            ),
+            default_root_object="index.html",
+        )
+    
