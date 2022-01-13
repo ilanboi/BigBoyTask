087b09aff1aa247e393151eedaa33ef1cbba50e6
ilanboi <97630631+ilanboi@users.noreply.github.com>
2022-01-13 03:57:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-#from aws_cdk import core
+from aws_cdk import core
 
 from static_web.static_web import StaticExampleStack
 
-#app = core.App()
-app = App()
+app = core.App()
 StaticExampleStack(app, "static-web")
 
 app.synth()
