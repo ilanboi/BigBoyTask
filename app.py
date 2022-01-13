80f709cb15eceee3d299bf779b0040aad6fa5b0a
ilanboi <97630631+ilanboi@users.noreply.github.com>
2022-01-13 02:18:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,22 @@
 from aws_cdk import (
-    core,
     aws_s3 as s3,
     aws_cloudfront as cloudfront
 )
 
-class StaticExampleStack(core.Stack):
- 
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
- 
-        self.bucket = s3.Bucket(self, "MyBucket",
-                           access_control=s3.BucketAccessControl.PUBLIC_READ,
-                           website_index_document="index.html"
-                           )
-        bucket.grant_public_access()
- 
-        self.distribution = cloudfront.Distribution(
-            self,
-            "cloudfront_distribution",
-            default_behavior=cloudfront.BehaviorOptions(
-                origin=origins.S3Origin(self.bucket),
-                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-            ),
-            default_root_object="index.html",
-        )
-    
+
+self.bucket = s3.Bucket(self, "MyBucket",
+                   access_control=s3.BucketAccessControl.PUBLIC_READ,
+                   website_index_document="index.html"
+                   )
+bucket.grant_public_access()
+
+self.distribution = cloudfront.Distribution(
+    self,
+    "cloudfront_distribution",
+    default_behavior=cloudfront.BehaviorOptions(
+        origin=origins.S3Origin(self.bucket),
+        viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    ),
+    default_root_object="index.html",
+)
+
