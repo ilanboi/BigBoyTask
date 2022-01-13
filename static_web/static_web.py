ab2a6040acb70cdc4e9929202dbe4f06ee684981
ilanboi <97630631+ilanboi@users.noreply.github.com>
2022-01-13 03:57:17 +0200

    Update static_web.py

--- static_web/static_web.py
+++ static_web/static_web.py
@@ -3,12 +3,11 @@ import time
 from aws_cdk import (
     aws_s3 as s3,
     aws_cloudfront as cf,
-    custom_resources as cr,
-    Stack
+    core
 )
 
 
-class StaticExampleStack(Stack):
+class StaticExampleStack(core.Stack):
 
     def __init__(self, scope: self.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
